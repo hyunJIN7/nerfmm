1be87ad821c6c339f8a61073c55296e2790fb3fd
hyunJIN7 <yunjingong12@naver.com>
2023-02-20 12:42:54 +0900

    feat eval

--- eval_for_image.py
+++ eval_for_image.py
@@ -37,7 +37,7 @@ from external.pohsun_ssim import pytorch_ssim
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_dir', type=str, default='./data_dir/nerfmm_release_data')
-    parser.add_argument('--scene_name', type=str, default='any_folder_demo/desk')
+    # parser.add_argument('--scene_name', type=str, default='any_folder_demo/desk')
     parser.add_argument('--test_img_num', type=int, default=-1, help='num of images to train')
 
 
@@ -48,9 +48,14 @@ def parse_args():
 def main(args):
     img_path = Path(os.path.join(args.base_dir, 'output'))
     gt_path =  Path(os.path.join(args.base_dir, 'gt'))
+    ours_path = Path(os.path.join(args.base_dir, 'ours_output'))
 
     n = args.test_img_num
-    test_gt_num = np.array([0])
+    test_index = np.array([1,55,110,165,220,274,329,348,441,495,550,607,661,716,770,825,879,935,989,1044])
+
+    """"
+        python eval_for_image.py --test_img_num=20 --base_dir=for_eval/h_5
+    """
 
     test_img = []
     gt_img = []
@@ -62,30 +67,19 @@ def main(args):
         test_img.append(imageio.imread(img_name))
         # test_gt_img.append(torch.from_numpy(imageio.imread(image_name)).float())
 
-        gt_img_name = '{}/{}'.format(img_path, '1'+str(test_gt_num[i]).zfill(5) + '.png')
+        gt_img_name = '{}/{}'.format(gt_path, '1'+str(test_index[i]).zfill(5) + '.png')
         # test_gt_img.append(torch.from_numpy(imageio.imread(image_name)))
         gt_img.append(imageio.imread(gt_img_name))
         # test_gt_img.append(torch.from_numpy(imageio.imread(image_name)).float())
 
-    # TEST GT evaluate
+    # DSNeRF TEST GT evaluate
     res = []
     import lpips
     lpips_loss = lpips.LPIPS(net="alex")
     # print('img size ', torch.from_numpy(imgs[0]).float().size())
     h,w,_ = torch.from_numpy(test_img[0]).float().size() #h,w,3
-    # print('test img size ', test_gt_img[0].float().size())
 
     for i in range(n):
-        #axis
-        # img = torch.from_numpy(imgs[i]).float().view(-1, h, w, 3).permute(0, 3, 1, 2)# [B,3,H,W]
-        # gt_img = test_gt_img[i] .view(-1, h, w, 3).permute(0, 3, 1, 2) # [B,3,H,W]
-        # psnr = PSNR(test_gt_img[i], imgs[i])
-        # #barf code
-        # img = torch.from_numpy(imgs[i]).float().view(-1, h, w, 3).permute(0, 3, 1, 2)# [B,3,H,W]
-        # gt_img = test_gt_img[i] .view(-1, h, w, 3).permute(0, 3, 1, 2) # [B,3,H,W]
-        # psnr = -10 * MSE_loss(img, gt_img).log10().item()  # mabye rendering,true image
-
-
         img = np.array(test_img[i]) / 255.0
         img = torch.from_numpy(img).float().view(-1, h, w, 3).permute(0, 3, 1, 2)# [B,3,H,W]
         gt_img = np.array(gt_img[i]) / 255.0
@@ -98,16 +92,58 @@ def main(args):
         # res.append(edict(psnr=psnr, ssim=ssim))
         break
 
-    print("--------------------------")
+    print("---------DSNeRF------------")
     print("PSNR:  {:8.2f}".format(np.mean([r.psnr for r in res])))
     print("SSIM:  {:8.2f}".format(np.mean([r.ssim for r in res])))
     print("LPIPS: {:8.2f}".format(np.mean([r.lpips for r in res])))
     print("--------------------------")
-    # dump numbers to file
-    quant_fname = "{}/quant.txt".format(args.ckpt_dir)
-    with open(quant_fname, "w") as file:
-        for i, r in enumerate(res):
-            file.write("{} {} {} {}\n".format(i, r.psnr, r.ssim, r.lpips))
+
+    gt_img = []
+    ours_img = []
+    '''load redner image'''
+    # imgs = (imgs.numpy() * 255).astype(np.uint8)
+    for i in range(n):
+        gt_img_name = '{}/{}'.format(gt_path, '1' + str(test_index[i]).zfill(5) + '.png')
+        # test_gt_img.append(torch.from_numpy(imageio.imread(image_name)))
+        gt_img.append(imageio.imread(gt_img_name))
+        # test_gt_img.append(torch.from_numpy(imageio.imread(image_name)).float())
+
+        gt_img_name = '{}/{}'.format(ours_path, 'rgb_' + str(i) + '.png')
+        # test_gt_img.append(torch.from_numpy(imageio.imread(image_name)))
+        ours_img.append(imageio.imread(gt_img_name))
+        # test_gt_img.append(torch.from_numpy(imageio.imread(image_name)).float())
+
+    # Ours TEST GT evaluate
+    res = []
+    import lpips
+    lpips_loss = lpips.LPIPS(net="alex")
+    # print('img size ', torch.from_numpy(imgs[0]).float().size())
+    h,w,_ = torch.from_numpy(ours_img[0]).float().size() #h,w,3
+    # print('test img size ', test_gt_img[0].float().size())
+
+    for i in range(n):
+        img = np.array(ours_img[i]) / 255.0
+        img = torch.from_numpy(img).float().view(-1, h, w, 3).permute(0, 3, 1, 2)# [B,3,H,W]
+        gt_img = np.array(gt_img[i]) / 255.0
+        gt_img = torch.from_numpy(gt_img).float().view(-1, h, w, 3).permute(0, 3, 1, 2) # [B,3,H,W]
+
+        psnr = -10 * MSE_loss(img, gt_img).log10().item()  # mabye rendering,true image
+        ssim = pytorch_ssim.ssim(img, gt_img).item()
+        lpips = lpips_loss(img* 2 - 1, gt_img * 2 - 1).item()
+        res.append(edict(psnr=psnr, ssim=ssim, lpips=lpips))
+        # res.append(edict(psnr=psnr, ssim=ssim))
+        break
+
+    print("--------Ours------------")
+    print("PSNR:  {:8.2f}".format(np.mean([r.psnr for r in res])))
+    print("SSIM:  {:8.2f}".format(np.mean([r.ssim for r in res])))
+    print("LPIPS: {:8.2f}".format(np.mean([r.lpips for r in res])))
+    print("--------------------------")
+
+
+
+
+
 
     return
 
